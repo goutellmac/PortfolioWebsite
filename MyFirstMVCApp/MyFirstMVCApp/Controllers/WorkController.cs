915b4d3480c8b968b830e7b7bf5370549e5d5227
Mac Goutell <goutellcode@gmail.com>
2015-05-18 13:48:46 -0600

    First Draft Level

---
+++ MyFirstMVCApp/MyFirstMVCApp/Controllers/WorkController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyFirstMVCApp.Controllers
+{
+    public class WorkController : Controller
+    {
+        // GET: Work
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Example1()
+        {
+            return View();
+        }
+    }
+}
